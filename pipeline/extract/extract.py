d8d4c4e2eae32fc7e80f2c2c8d4a6dc9a8400c4a
Wojciech Dranka <dr.woj@wp.pl>
2023-11-19 00:29:41 +0100

    create extract module for geolife and hannover datasets

---
+++ pipeline/extract/extract.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+
+
+def extract_geolife(path_plt):
+    path_txt = os.path.splitext(path_plt)[0] + '.txt'
+    os.rename(path_plt, path_txt)
+
+    df = pd.read_csv(path_txt,
+                     skiprows=6,
+                     header=None,
+                     usecols=[0, 1, 4],
+                     names=['latitude', 'longitude', 'timestamp'])
+
+    os.rename(path_txt, path_plt)
+    return df
+
+
+def extract_hannover(path_csv):
+    df = pd.read_csv(path_csv) \
+        .rename(columns={'trip_id': 'trajectory_id'})
+    return df
