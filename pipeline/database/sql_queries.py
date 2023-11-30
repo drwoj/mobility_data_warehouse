94856a2b2f268eca1d9d3739a5c0a7de31606336
Wojciech Dranka <dr.woj@wp.pl>
2023-11-30 12:35:06 +0100

    add center_point column to trajectory

--- pipeline/database/sql_queries.py
+++ pipeline/database/sql_queries.py
@@ -31,13 +31,14 @@ FROM generate_series(
 """
 
 create_trajectories_from_points = """
-SELECT
+SELECT 
 	id,
 	startTimestamp(route)::timestamp AS date,
 	country,
-	route,
+	AsText(route),
 	length(route)/1000 AS distance,
 	duration(route) AS duration,
-	twAvg(speed(route)) * 3.6 AS avg_speed
+	twAvg(speed(route)) * 3.6 AS avg_speed,
+	ST_AsText(ST_Centroid(trajectory(route))) AS center_point
 FROM trajectory
 """
