7c9c03ba6fa1e8c22ebe7b52aa54bd2f54c3a284
Wojciech Dranka <dr.woj@wp.pl>
2023-12-04 20:10:51 +0100

    move etl_process to main_etl function and call it from __init__.py

--- pipeline/etl/__init__.py
+++ pipeline/etl/__init__.py
@@ -2,10 +2,10 @@ from database.connectors.MobilityDWConnector import MobilityDWConnector
 from database.sql_queries import insert_date, drop_mobility_db, create_mobility_dw
 import etl.extract.extract as e
 import etl.transform.transform as t
-import paths as p
+from etl.etl_process import main_etl
 
 
-def run_etl():
+def prepare_etl():
     with MobilityDWConnector() as connector:
         connector.execute_query(drop_mobility_db)
         connector.execute_query(create_mobility_dw)
@@ -13,4 +13,5 @@ def run_etl():
 
 
 if __name__ == "__main__":
-    run_etl()
+    prepare_etl()
+    main_etl()
