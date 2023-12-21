e294abd1a16a43507fcad3b114c257bf5650df8d
Wojciech Dranka <dr.woj@wp.pl>
2023-12-21 23:20:47 +0100

    change paths.py to let user specify datasets_path

--- pipeline/paths.py
+++ pipeline/paths.py
@@ -1,9 +1,13 @@
-path_trajectories_hannover = r'C:\Users\drwoj\Desktop\inzynierka\datasets\trajectories_hannover.csv'
-path_trajectories_geolife = r"C:\Users\drwoj\Desktop\inzynierka\datasets\trajectories_china\Geolife Trajectories 1.3\Data"
+import os
 
-path_weather_hannover = r'C:\Users\drwoj\Desktop\inzynierka\datasets\weather_hannover.csv'
-path_weather_beijing = r'C:\Users\drwoj\Desktop\inzynierka\datasets\weather_beijing.csv'
-path_regions = r'C:\Users\drwoj\Desktop\inzynierka\datasets\regions.xlsx'
-path_fuel_hannover = r'C:\Users\drwoj\Desktop\inzynierka\datasets\fuel_prices_hannover.xlsx'
-path_fuel_beijing = r'C:\Users\drwoj\Desktop\inzynierka\datasets\fuel_prices_china.xlsx'
-path_economy = r'C:\Users\drwoj\Desktop\inzynierka\datasets\economy_indicators.xlsx'
+DATASETS_PATH = r'C:\Users\\drwoj\Desktop\inzynierka\datasets'
+
+path_trajectories_hannover = os.path.join(DATASETS_PATH, 'trajectories_hannover.csv')
+path_trajectories_geolife = os.path.join(DATASETS_PATH, 'trajectories_china', 'Geolife Trajectories 1.3', 'Data')
+
+path_weather_hannover = os.path.join(DATASETS_PATH, 'weather_hannover.csv')
+path_weather_beijing = os.path.join(DATASETS_PATH, 'weather_beijing.csv')
+path_regions = os.path.join(DATASETS_PATH, 'regions.xlsx')
+path_fuel_hannover = os.path.join(DATASETS_PATH, 'fuel_prices_hannover.xlsx')
+path_fuel_beijing = os.path.join(DATASETS_PATH, 'fuel_prices_china.xlsx')
+path_economy = os.path.join(DATASETS_PATH, 'economy_indicators.xlsx')
